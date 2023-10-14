3d77ff8a33d01f3e6664f11f7306e6af098b91cd
Vikram <singh.vikramsingh06@gmail.com>
2023-10-13 23:50:52 -0400

    remove console

--- src/App.js
+++ src/App.js
@@ -5,45 +5,7 @@ import Todo from "./components/Todo";
 
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      todos: [],
-      newTodo: "",
-    };
-  }
-  addTodo = (task) => {
-    this.setState((prev) => {
-      return {
-        todos: [...prev.todos, ,prev.newTodo],
-        newTodo: "",
-      };
-    });
-  };
-  handleInputChange = (event) => {
-    this.setState({ newTodo: event?.target?.value });
-  };
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (this.state.newTodo?.trim() === "") return;
-    // this.addTodo(this.state.newTodo);
-    this.setState((prev) => {
-      return {
-        todos: [...prev.todos, prev.newTodo],
-        newTodo: "",
-      };
-    });
-  };
-  removeTodo = (index) => {
-    const updateTodos = [...this.state.todos];
-    updateTodos?.splice(index, 1);
-    console.log("dafadfadf", updateTodos);
-    this.setState({ todos: updateTodos });
-  };
-
   render() {
-  console.log(this.state.todos)
-
     return (
       <div className='App'>
         <Todo />
