5b831663ad10c4fdd0ce6a18594a06212419bfa6
Vikram <singh.vikramsingh06@gmail.com>
2023-10-19 15:00:51 -0400

    lab 7

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import React, {useState } from "react";
 import "./App.css";
 import Todo from "./components/Todo";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Navigate,
+} from "react-router-dom";
+import Dashboard from "./components/Dashborad";
+import Login from "./components/Login";
 
+const UserProfile = ({ match }) => {
+  console.log(match);
+  const userId = match?.params?.id;
+  return (
+    <div>
+      <h2>User Profile</h2>
+      <p>User Id :{userId}</p>
+    </div>
+  );
+};
+const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <Todo />
-      </div>
-    );
-  }
-}
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path='/dashboard'
+          element={loggedIn ? <Dashboard /> : <Navigate to='/login' />}
+        />
+        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Navigate to='/dashboard' />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
